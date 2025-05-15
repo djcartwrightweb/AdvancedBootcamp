f55ba50f663bd633592660c3b169413e960ec180
DJ Cartwright <dj.cartwright.web@gmail.com>
2025-05-15 13:33:44 -0400

    Initial Commit

---
+++ AdvancedBootcamp/AdvancedBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedBootcampApp.swift
+//  AdvancedBootcamp
+//
+//  Created by David Cartwright on 2025-05-15.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
